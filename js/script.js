7556ab1f86fc8855a73a9ae4553bc84d23cfef34
Hgoatly <hgoatly@hotmail.com>
2020-09-07 09:40:53 +0000

    landing page hidden onclick function added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let y = document.getElementById("main-content");
+y.style.display = "none";
+
+function hideLandingPage() {
+ let x = document.getElementById("landing-page");
+ x.style.display = "none";
+ y.style.display = "block";
+}
