bf43dfc2c047b8f0af3670ab686023c029da4ad2
Hgoatly <hgoatly@hotmail.com>
2020-09-23 19:20:00 +0000

    jquery card-front, card-back toggle method added to 'lakes' card

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function hideLandingPage() {
  x.style.display = "none";
  y.style.display = "block";
 }
-
+/*
 let lakeMap = document.getElementById("lake-map")
 let lakes = document.getElementById("lakes");
 lakeMap.style.display = "none";
@@ -16,19 +16,20 @@ function showMap() {
     let mapButtons = document.getElementsByClassName("map-buttons");
  /*   let hideMapButtons = document.getElementsByClassName("hide-map-buttons"); */
 
-
+/*
 if (lakeMap.style.display === "none") {
     lakeMap.style.display = "block";
     lakes.style.display = "none";
 /*    mapButtons.style.display = "none";
-    hideMapButtons.style.display = "block"; */
-    mapButtons[0].innerHTML = "Hide Map"; 
+    hideMapButtons.style.display = "block"; 
+    mapButtons[0].innerHTML = "Hide Map"; */
+/*
 } else {
     lakeMap.style.display = "none";
     lakes.style.display = "block";
  /*   mapButtons.style.display = "block";
     hideMapButtons.style.display = "none"; */
-    mapButtons[0].innerHTML = "View on Map";
+  /*  mapButtons[0].innerHTML = "View on Map";
 }
 }
 
@@ -65,3 +66,33 @@ function showForestsMap() {
         mapButtons[0].innerHTML = "View on Map";
     }
 }
+
+*/
+/*
+const cards = document.querySelectorAll(".cards-single");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+cards.forEach((card) => card.addEventListener("click", flipCard)); 
+*/
+/*
+const cardFront = document.querySelectorAll(".card-front")
+
+function showMap() {
+  this.classList.toggle("hide");
+}
+*/
+
+$(document).ready(function(){
+    $("#maps-button-1").click(function(){
+        $(".card-front").toggle();
+        $(".card-back").toggle();
+       
+    })
+
+    $(".map-buttons").click(function(){
+        $(".map-buttons").text("Hide Map");
+        
+    })
+});
