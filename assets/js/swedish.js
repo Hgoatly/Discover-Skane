8eedb078f99f728575d62ff9e2805beeb521c805
Hgoatly <hgoatly@hotmail.com>
2020-10-09 12:45:57 +0000

    Refactor swedish.js code to toggle languages using classes in JQuery

--- assets/js/swedish.js
+++ assets/js/swedish.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
 
-//Toggle card pictures and maps to be shown and hidden.
+//Toggle between Swedish and English language when the relevant flag is clicked.
     $("#swedish").click(function(){
         $("#swedish-flag").toggle();
         $("#uk-flag").toggle();
-        $("#intro-heading").toggle();
-        $("#intro-heading-swedish").toggle();
-        $("#intro").toggle();
-        $("#intro-swedish").toggle();
+        $(".english-content").toggle();
+        $(".swedish-content").toggle();    
+        })
 
-         })
-    
 })
